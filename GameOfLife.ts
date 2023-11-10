f7315a9ceb23995b2995002afef5018bd7a9bbf1
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-11-10 08:01:20 +0100

    Update grouping cells

--- GameOfLife.ts
+++ GameOfLife.ts
@@ -1,3 +1,6 @@
+import Cell from "./Cell";
+import { countBy } from "ramda";
+
 class GameOfLife {
   readonly rows;
   readonly columns;
@@ -5,5 +8,18 @@ class GameOfLife {
   constructor(rows: number, columns: number) {
     this.rows = rows;
     this.columns = columns;
+  }
+
+  generateCells(cells: Cell[]): Cell[] {
+    const counter = countBy((cell: Cell) => cell.x + cell.y);
+    console.log(counter(cells));
+    return cells.flatMap(cell => this.getNeighboringCells(cell))
+  }
+
+  private getNeighboringCells(cell: Cell): Cell[] {
+    return [cell.x - 1, cell.x, cell.x + 1].flatMap(x =>
+      [cell.y - 1, cell.y, cell.y + 1]
+        .filter(y => cell.x != x && cell.y != y)
+        .map(y => Cell.of(x, y)));
   }
 }
