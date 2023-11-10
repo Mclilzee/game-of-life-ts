2a0a5b3affbb0e7ea5065c4788419129a023ec29
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-11-10 08:41:17 +0100

    Remove classes and use functions only

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
-import Cell from "./Cell";
-import GameOfLife from "./GameOfLife";
-const gameOfLife = new GameOfLife(8, 8);
+import { countBy } from "ramda";
 
-const testCells = [Cell.of(1, 2), Cell.of(1, 2), Cell.of(2, 1), Cell.of(3, 2)];
+type Cell = [number, number];
 
-gameOfLife.generateCells(testCells)
+function generateCells(cells: Cell[]) {
+  const counter = countBy((cell: Cell) => cell[0].toString() + cell[1].toString());
+  return counter(cells.flatMap(cell => getNeighboringCells(cell)));
+}
+
+function getNeighboringCells(cell: Cell) {
+  return [cell[0] - 1, cell[0], cell[0] + 1].flatMap(x =>
+    [cell[1] - 1, cell[1], cell[1] + 1]
+      .filter(y => !(cell[0] == x && cell[1] == y))
+      .map(y => [x, y] as Cell));
+}
