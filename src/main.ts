04184ab6b241cd237ea6f864ef422eb112e942c2
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-11-11 12:26:34 +0100

    Add adjustments

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import "./style.css";
 import { range } from "ramda";
 import Cell from "./Cell";
 import updateCells from "./updateCells";
+
 const rowSize = 100;
 const columnSize = 100;
 
-const generateCell = (x: number, y: number) => Cell.of(x, y, Math.random() < 0.05 ? true : false);
+const generateCell = (x: number, y: number) => Cell.of(x, y, Math.random() >= 0.5 ? true : false);
 
 const cells = range(0, rowSize).flatMap(x => range(0, columnSize).map(y => generateCell(x, y)));
 
@@ -15,28 +16,30 @@ let cellElements = cells.map(cell => {
   if (cell.alive) {
     element.classList.add("alive");
   }
-
   return element;
 });
 
 const canvas = document.querySelector(".canvas") as HTMLDivElement;
+canvas.style.display = "grid";
+canvas.style.gridTemplateRows = `repeat(${rowSize}, 1fr)`;
+canvas.style.gridTemplateColumns = `repeat(${columnSize}, 1fr)`;
 cellElements.forEach(node => canvas.appendChild(node))
 
 function displayChange() {
   updateCells(cells);
-  cells.forEach((cell, x) => {
-    if (cell.alive) {
-      cellElements[x].classList.add("alive")
+  for (let i = 0; i < cells.length; i++) {
+    if (cells[i].alive) {
+      cellElements[i].classList.add("alive");
     } else {
-      cellElements[x].classList.remove("alive");
+      cellElements[i].classList.remove("alive");
     }
-  })
+  }
 
   setTimeout(() => {
     if (cells.find(cell => cell.alive)) {
       displayChange();
     }
-  }, 100);
+  }, 80);
 }
 
 displayChange();
