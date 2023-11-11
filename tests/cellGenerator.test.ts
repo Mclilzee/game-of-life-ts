f314dbeccb64be20b6574c765ff68f8d90289218
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-11-11 10:02:29 +0100

    Add test for block cells

--- tests/cellGenerator.test.ts
+++ tests/cellGenerator.test.ts
@@ -1,9 +1,21 @@
-import generateCells, { Cell } from "../cellGenerator";
+import generateCells from "../cellGenerator";
+import Cell from "../Cell";
 import { expect, describe, test } from "bun:test";
 
 describe("Generate a map of live cells", () => {
   test("If empty list provided, it returns empty", () => {
-    const aliveCells: Cell[][] = [];
-    expect(generateCells(aliveCells)).toEqual([]);
+    const cells: Cell[] = [];
+    expect(generateCells(cells)).toEqual([]);
+  });
+
+  test("Return correct block", () => {
+    const cells = [
+      Cell.of(0, 0, true),
+      Cell.of(0, 1, true),
+      Cell.of(1, 0, true),
+      Cell.of(1, 1, true),
+    ]
+
+    expect(generateCells(cells)).toEqual(cells);
   });
 });
