fc979f818c4360eb6b0616d778989d971c6818ed
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-11-11 09:25:31 +0100

    Remove old tests

--- tests/cellGenerator.test.ts
+++ tests/cellGenerator.test.ts
@@ -1,25 +1,9 @@
 import generateCells from "../cellGenerator";
 import { expect, describe, test } from "bun:test";
 
-describe("Generate a map of cells that have adjacent live cells", () => {
-  test("If empty list provided, it returns an empty map", () => {
+describe("Generate a map of live cells", () => {
+  test("If empty list provided, it returns empty", () => {
     const aliveCells: [number, number][] = [];
     expect(generateCells(aliveCells)).toEqual({});
-  });
-
-  test("Returns correct adjacunt cells that are touching", () => {
-    const aliveCells: [number, number][] = [[2, 2]];
-    const expectedMap = {
-      "11": 1,
-      "12": 1,
-      "13": 1,
-      "21": 1,
-      "23": 1,
-      "31": 1,
-      "32": 1,
-      "33": 1,
-    }
-
-    expect(generateCells(aliveCells)).toEqual(expectedMap);
   });
 });
