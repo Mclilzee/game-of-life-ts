9ea29169ff6c47fff4ac8c4936dff62d853598b2
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-08 11:07:04 +0100

    Clean up elements creation

--- src/main.ts
+++ src/main.ts
@@ -10,24 +10,14 @@ const generateCell = (x: number, y: number) => Cell.of(x, y, Math.random() >= 0.
 
 const cells = range(0, rowSize).flatMap(x => range(0, columnSize).map(y => generateCell(x, y)));
 
-let cellElements = cells.map(cell => {
-  const element = document.createElement("div");
-  element.classList.add("cell");
-  if (cell.alive) {
-    element.classList.add("alive");
-  }
-  return element;
-});
-
 const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2Dcanvas;
 
 function render() {
   updateCells(cells);
   for (let i = 0; i < cells.length; i++) {
     if (cells[i].alive) {
-      cellElements[i].classList.add("alive");
     } else {
-      cellElements[i].classList.remove("alive");
     }
   }
 
