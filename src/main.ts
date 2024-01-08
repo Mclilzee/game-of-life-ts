2b4625003f22041c9b1e15a7bb2c9591c2e47e46
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-08 11:21:58 +0100

    Add canvas drawing of cells

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { range } from "ramda";
 import Cell from "./Cell";
 import updateCells from "./updateCells";
 
-const rowSize = 100;
+const rowSize = 32;
 const columnSize = rowSize;
 
 const generateCell = (x: number, y: number) => Cell.of(x, y, Math.random() >= 0.5 ? true : false);
@@ -11,13 +11,20 @@ const generateCell = (x: number, y: number) => Cell.of(x, y, Math.random() >= 0.
 const cells = range(0, rowSize).flatMap(x => range(0, columnSize).map(y => generateCell(x, y)));
 
 const canvas = document.querySelector("canvas") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d") as CanvasRenderingContext2Dcanvas;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
 function render() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
   updateCells(cells);
+
+  ctx.fillStyle = "black";
+  const width = canvas.width / columnSize;
+  const height = canvas.height / rowSize;
   for (let i = 0; i < cells.length; i++) {
-    if (cells[i].alive) {
-    } else {
+    const cell = cells[i];
+    if (cell.alive) {
+      ctx.fillRect(cell.x * width, cell.y * height, width, height);
     }
   }
 
