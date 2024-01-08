5c23c06a1c1f33b7a6ef774ab1e9895c9100ef7d
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-08 11:25:00 +0100

    Fix flooring widht and height causing problems

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { range } from "ramda";
 import Cell from "./Cell";
 import updateCells from "./updateCells";
 
-const rowSize = 32;
+const rowSize = 100;
 const columnSize = rowSize;
 
 const generateCell = (x: number, y: number) => Cell.of(x, y, Math.random() >= 0.5 ? true : false);
@@ -12,6 +12,9 @@ const cells = range(0, rowSize).flatMap(x => range(0, columnSize).map(y => gener
 
 const canvas = document.querySelector("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+// canvas.addEventListener("click", (e) => {
+//
+// });
 
 function render() {
   ctx.fillStyle = "white";
@@ -32,7 +35,7 @@ function render() {
     if (cells.find(cell => cell.alive)) {
       render();
     }
-  }, 80);
+  }, 100);
 }
 
 render();
